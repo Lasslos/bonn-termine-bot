809387a4d7e62b2392ace9ceb05eb30bb310e397
lasloh <git@laslo-hauschild.eu>
2024-04-29 08:23:40 +0200

    Minimal working example

--- main.py
+++ main.py
@@ -2,21 +2,11 @@ import requests
 import logging
 import re
 import json
+import http.client as http_client
 
-
-# These two lines enable debugging at httplib level (requests->urllib3->http.client)
-# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
-# The only thing missing will be the response.body which is not logged.
 
 def enable_debug():
-    try:
-        import http.client as http_client
-    except ImportError:
-        # Python 2
-        import httplib as http_client
     http_client.HTTPConnection.debuglevel = 1
-
-    # You must initialize logging, otherwise you'll not see debug output.
     logging.basicConfig()
     logging.getLogger().setLevel(logging.DEBUG)
     requests_log = logging.getLogger("requests.packages.urllib3")
@@ -25,70 +15,46 @@ def enable_debug():
 
 
 def get_appointments():
-    # Request base url
-
-    base_url = "https://termine.bonn.de/m/dlz/extern/calendar/?uid=b91bb67b-15cf-44df-ab0b-96ebe25c1ae3"
-    # Headers
-    light_headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
-        "Host": "termine.bonn.de",
-        "Origin": base_url,
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Accept-Language": "de,en-CA;q=0.8,en-US;q=0.5,en;q=0.3",
-        "Accept-Encoding": "gzip, deflate, br",
-    }
-
-    response = requests.get(base_url, headers=light_headers, allow_redirects=False)
+    domain = "https://termine.bonn.de"
+    response = requests.get(domain + "/m/dlz/extern/calendar/?uid=b91bb67b-15cf-44df-ab0b-96ebe25c1ae3", allow_redirects=False)
     # Base url should return 302 with 'wsid' as a parameter in the url
-    # urllib automatically goes to 302 location
-
     if response.status_code != 302:
-        print("Base url did not return 302")
+        print("Couldn't get wsid token")
         exit()
-    print("Got correct 302")
 
     # Get parameter from the url
-    base_url = "https://termine.bonn.de" + response.headers["Location"]
-    cookie_token = response.cookies.get_dict()["__RequestVerificationToken"]
-
+    base_url = domain + response.headers["Location"]
     # Only send cookies "__RequestVerificationToken" and "ASP.NET_SessionId"
     cookies = {
-        "__RequestVerificationToken": cookie_token,
+        "__RequestVerificationToken": response.cookies.get_dict()["__RequestVerificationToken"],
         "ASP.NET_SessionId": response.cookies.get_dict()["ASP.NET_SessionId"],
     }
 
-    response2 = requests.get(base_url, cookies=cookies, headers=light_headers, allow_redirects=False)
+    response2 = requests.get(base_url, cookies=cookies, allow_redirects=False)
 
     # Load request token
     pattern = r"^(?:.*)<input type='hidden' id='RequestVerificationToken' name='__RequestVerificationToken' value='(.*?)' />"
     match = re.search(pattern, response2.text, flags=re.MULTILINE)
-
     if match:
-        token = match.group(1)
-        print("Request 2 contained matching token")
+        form_token = match.group(1)
     else:
-        print("String not found")
+        print("Couldn't get form token")
         exit()
 
     # Load form data file
     form_data = open("form-data.txt", "r")
     form_data = form_data.read()
-    form_data = "__RequestVerificationToken=" + token + form_data
+    form_data = "__RequestVerificationToken=" + form_token + form_data
 
     # Send post to base_url with form_data
-
-    headers = light_headers.copy()
-    headers["Content-Type"] = "application/x-www-form-urlencoded"
-    response3 = requests.post(base_url, data=form_data, cookies=cookies, headers=headers, allow_redirects=False)
-    print("Response 3 success: ", response3.status_code)
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    requests.post(base_url, data=form_data, cookies=cookies, headers=headers, allow_redirects=False)
 
     # Finally, get the appointments
-
     url = base_url.split("?")[0] + "search_result?search_mode=earliest&" + base_url.split("?")[1]
-    response4 = requests.get(url, cookies=cookies, headers=light_headers, allow_redirects=False)
+    response4 = requests.get(url, cookies=cookies, allow_redirects=False)
 
     pattern = r"(?<=<div id=\"json_appointment_list\">).*?(?=</div>)"
-
     match = re.search(pattern, response4.text, flags=re.DOTALL)
 
     if match:
